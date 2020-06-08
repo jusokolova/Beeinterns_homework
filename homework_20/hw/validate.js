6eebaf3298f493df9769855d2b5c77950f6ed712
Julia <60842377+jusokolova@users.noreply.github.com>
2020-06-08 13:13:08 +0300

    'Поправки'

--- homework_20/hw/validate.js
+++ homework_20/hw/validate.js
@@ -1,23 +1,21 @@
 function validate(data) {
-    let { login, password, confirmPassword, license, firstName, genderValue, input, value } = data;
+    const { login, password, confirmPassword, license, firstName, input, } = data;
     input = document.getElementsByName('gender');
     if (!login || !password) {
         alert('Укажите логин/пароль');
     } else if (login == 'beeline' || login == 'beeinterns' || login == 'bee') {
         alert('Логин занят');
-    }
-    else if (password.length < 6) {
+    } else if (password.length < 6) {
         alert('Пароль должен быть длинной не менее 6 символов');
     } else if (password !== confirmPassword) {
         alert('Пароли должны совпадать');
     } else if (!firstName) {
         alert('Укажите Имя');
-    }
-     else if (!license) {
+    } else if (!license) {
         alert('Необходимо согласие');
-    } else if (input[0].value == 'male' && input[0].checked == true) {
+    } else if (data.gender == 'male' && input[0].checked == true) {
         alert(`Уважаемый ${firstName}, заявка создана`);
-    } else if (input[1].value == 'female' && input[1].checked == true) {
+    } else if (data.gender == 'female' && input[1].checked == true) {
         alert(`Уважаемая ${firstName}, заявка создана`);
     }
 }
